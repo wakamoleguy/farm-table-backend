98fa5166674a17e4c2e88a08037cbfd9ff407d01
Will Mitchell <wakamoleguy@gmail.com>
2017-07-29 10:15:40 -0700

    Mongo working?

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
 const router = require('./router');
 
@@ -9,6 +8,8 @@ const router = require('./router');
 // Set up MongoDB
 const mongoose = require('mongoose');
 const uristring = process.env.MONGODB_URI;
+console.log(uristring);
+
 mongoose.connect(uristring);
 
 const port = process.env.PORT || 3030;
